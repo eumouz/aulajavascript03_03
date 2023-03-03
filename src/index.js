955d53663e7f8d4e286ae3bb81107689a9b7e341
eumouz <125469962+eumouz@users.noreply.github.com>
2023-03-03 11:16:45 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+/*
+o if e o else são estruturas condicionais neste caso ,
+o if espera que uma condição seja atendida para que parte 
+do código seja executado caso não ocorra com sucesso a condição do if
+o bloco else sera adicionado*/
+
+let nota = 10
+
+if(nota >=7 && nota <= 10){
+  console.log('passou de ano')
+}
+
+else if (nota < 7 && nota >=0){
+  console.log('aluno reprovado')
+}
+
+else{
+  console.log('nota informada errado')
+}
+
+
+
+
